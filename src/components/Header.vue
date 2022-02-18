e598a824ce64f9d0b8af8b81028c97ae0fb3f8dd
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-18 18:00:48 +0100

    Adds imgs + new components (Header, Main, Footer)

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav>
+      <div>
+        <!-- img -->
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Culinary History</li>
+        <li>Our Team</li>
+        <li>Our Menu</li>
+        <li>Takeout</li>
+        <li>Bulletin</li>
+        <li>Reservations</li>
+        <li>(Shop icon)</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>
