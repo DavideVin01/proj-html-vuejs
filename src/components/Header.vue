a43cee7fb212014cfebe33f63e60333181ae1ef4
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-18 18:26:20 +0100

    Adds nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,26 @@
 <template>
   <header>
-    <nav>
+    <!-- Navigation bar | New Component -->
+    <nav
+      class="d-flex justify-content-between align-items-center py-3 container"
+    >
       <div>
-        <!-- img -->
+        <img
+          src="../assets/imgs/logo-restaurant.png"
+          alt="AVADA RESTAURANT LOGO"
+        />
       </div>
-      <ul>
-        <li>Home</li>
-        <li>Culinary History</li>
-        <li>Our Team</li>
-        <li>Our Menu</li>
-        <li>Takeout</li>
-        <li>Bulletin</li>
-        <li>Reservations</li>
-        <li>(Shop icon)</li>
+      <ul class="d-flex">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Culinary History</a></li>
+        <li><a href="#">Our Team</a></li>
+        <li><a href="#">Our Menu</a></li>
+        <li><a href="#">Takeout</a></li>
+        <li><a href="#">Bulletin</a></li>
+        <li><a href="#">Reservations</a></li>
+        <li>
+          <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </li>
       </ul>
     </nav>
   </header>
@@ -24,5 +32,24 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "../assets/scss/style.scss";
+header {
+  background-color: $black;
+}
+
+nav {
+  ul {
+    li {
+      padding: 0 15px;
+      list-style-type: none;
+      margin-top: 15px;
+      a {
+        text-decoration: none;
+        color: $white;
+        font-size: 14px;
+      }
+    }
+  }
+}
 </style>
