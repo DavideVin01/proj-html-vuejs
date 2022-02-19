f6a66fc51fdf59c96a42fa4dbf840f4ed7876553
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-19 15:20:12 +0100

    Creates first section structure (row cols)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,27 @@
 <template>
-  <main></main>
+  <main>
+    <!-- Food is our common ground section -->
+    <section id="common-ground" class="bg-white">
+      <div class="small-container">
+        <div class="row">
+          <div class="col-6">
+            <div class="paragraph-text">
+              <strong>HAC TELLUS, FELIS RISUS AT</strong> mattis mattis. Eget
+              euismod semper eget tortor, donec amet, blandit. Tristique
+              facilisi faucibus elementum feugiat in nam in feugiat. Ipsum odio
+              etiam duis facilis amet vulputate.
+            </div>
+          </div>
+          <div class="col-6"></div>
+        </div>
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-4"></div>
+          <div class="col-4"></div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -8,5 +30,5 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 </style>
