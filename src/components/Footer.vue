17e3320906d4cc6e3d108883139f3bfa570e389e
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-21 17:25:17 +0100

    Adds footer elements + adds logos icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,73 @@
 <template>
   <footer>
-    <div class="container"></div>
+    <div
+      class="
+        mini-container
+        text-white
+        d-flex
+        align-items-center
+        justify-content-center
+        text-center
+        flex-column
+      "
+    >
+      <div class="py-4">
+        <img
+          src="../assets/imgs/logo-restaurant.png"
+          alt="AVADA RESTAURANT LOGO"
+        />
+      </div>
+      <p class="paragraph-text text-silver">
+        Quam eu proin sit massa condimentum. Volupat non pulvinar aliquet nunc.
+        Quam eu proin sit massa condimentum.
+      </p>
+      <div id="logos" class="py-5 mb-5">
+        <img
+          class="logo"
+          src="../assets/imgs/logos/facebook.png"
+          alt="Facebook"
+          role="button"
+        />
+        <img
+          class="logo"
+          src="../assets/imgs/logos/twitter.png"
+          alt="twitter"
+          role="button"
+        />
+        <img
+          class="logo"
+          src="../assets/imgs/logos/youtube.png"
+          alt="youtube"
+          role="button"
+        />
+        <img
+          class="logo"
+          src="../assets/imgs/logos/instagram.png"
+          alt="instagram"
+          role="button"
+        />
+        <img
+          class="logo"
+          src="../assets/imgs/logos/linked.png"
+          role="button"
+          alt="linked"
+        />
+      </div>
+    </div>
+    <div
+      id="copyright"
+      class="
+        bg-black
+        text-center text-small text-silver
+        d-flex
+        justify-content-center
+        align-items-center
+        h-25
+      "
+    >
+      Copyright 2012 - 2019 Avada | All Rights Reserved | Powered by
+      <strong class="text-white" role="button"> WordPress | ThemeFusion</strong>
+    </div>
   </footer>
 </template>
 
@@ -18,5 +85,10 @@ footer {
   background-position: bottom center;
   height: 550px;
   width: 100%;
+  padding: 100px 0;
+}
+.logo {
+  width: 50px;
+  margin: 0 10px;
 }
 </style>
