400b1ce6b8b30c810cc0ea624421e04a73cc0d98
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-21 11:48:30 +0100

    Adds new utilities + Latest news section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -212,6 +212,55 @@
         </div>
       </div>
     </section>
+
+    <!-- LATEST NEWS & PROMOTIONS -->
+    <section id="latest-news">
+      <div class="container">
+        <h2 class="my-5 py-4 cursive-text text-center">
+          Latest news & promotions
+        </h2>
+        <div class="row mb-5 pb-3">
+          <!-- Left col -->
+          <div class="col-4 d-flex flex-column align-items-center">
+            <img
+              src="../assets/imgs/blog92x-400x458.jpg"
+              class="mb-4"
+              alt="Coffee"
+            />
+            <div class="h4 font-bebas">THE BEST COFFEE IN TOWN</div>
+            <figcaption class="text-small">
+              By admin | January 7th, 2020 | Categories: News
+            </figcaption>
+          </div>
+
+          <!-- Center col -->
+          <div class="col-4 d-flex flex-column align-items-center">
+            <img
+              src="../assets/imgs/blog72x-400x458.jpg"
+              class="mb-4"
+              alt="Coffee"
+            />
+            <div class="h4 font-bebas">THE BEST COFFEE IN TOWN</div>
+            <figcaption class="text-small">
+              By admin | January 7th, 2020 | Categories: News
+            </figcaption>
+          </div>
+
+          <!-- Right col -->
+          <div class="col-4 d-flex flex-column align-items-center">
+            <img
+              src="../assets/imgs/blog82x-400x458.jpg"
+              class="mb-4"
+              alt="Coffee"
+            />
+            <div class="h4 font-bebas">THE BEST COFFEE IN TOWN</div>
+            <figcaption class="text-small">
+              By admin | January 7th, 2020 | Categories: News
+            </figcaption>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
