1b8ea6347ebc42fc5d08b44dbb81aa12a558d80d
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-21 11:59:39 +0100

    Adds jumbotron image + text

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -261,6 +261,36 @@
         </div>
       </div>
     </section>
+
+    <!-- TAKEOUT NOW AVAILABLE -->
+    <section id="takeout">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-7 p-0">
+            <div id="takeout-jumbotron"></div>
+          </div>
+          <div class="col-5 bg-black py-5 ps-2">
+            <div class="p-5 ms-5">
+              <div class="subtitle pb-3 pt-5">ENJOY YOUR MEAL AT HOME</div>
+              <div class="section-title text-white py-3">
+                TAKEOUT NOW<br />
+                AVAILABLE
+              </div>
+              <p class="text-silver py-3 paragraph-text">
+                Pellentesque vitae viverra risus, sagittis. <br />Venenatis
+                ridiculus scelerisque nisi in urna nulla.<br />
+                Sit tempor a et nisl, ac felis.
+              </p>
+              <div class="pt-3 pb-5">
+                <button class="text-white border-white">
+                  VIEW TAKEOUT MENU
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -286,6 +316,15 @@ h3 {
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  height: 100%;
+  width: 100%;
+}
+
+#takeout-jumbotron {
+  background-image: url("../assets/imgs/slider72x-scaled.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: bottom;
   height: 100%;
   width: 100%;
 }
